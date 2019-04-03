71af2e66e73a0a76446c38dbda4f9e72a0393cbd
warrenjayv <wbv4@njit.edu>
2019-04-02 20:04:25 -0400

    Tue Apr  2 20:04:32 EDT 2019

--- back/SubmitTest2.php
+++ back/SubmitTest2.php
@@ -1,9 +1,24 @@
 <?php
+date_default_timezone_set("America/New_York"); 
 
 include 'dblogin_interface.php';
+include 'autolog.php'; 
 
+$target = '/afs/cad/u/w/b/wbv4/public_html/Middle/tracklogs/addA.txt'; 
 $response   = file_get_contents('php://input');
 $decoder    = json_decode($response, true);
+
+$write = "[+] addA page accessed " . date("Y-m-d h:i:sa") . "\n";  
+$write .= "page is received data. processing the data below:\n"; autolog($write, $target); 
+$write = print_r($decoder, true);
+  if (empty($write)) {
+      $write = "addA decoder is empty! \n"; autolog($write, $target); 
+			$report = array('type' => 'addA', 'error' => 'addA decoder received no data'); 
+			echo json_encode($report); 
+			return 0; //program terminates and code stops here. 
+  } else {
+  autolog($write, $target); 
+	}
 
 /*
 UPDATE: 
@@ -18,28 +33,37 @@ ANSWER:
  
 if (! $feedback = submitExam($conn, $decoder)) { //calls the function getQUEST() ; 
       $error = "backend submitExam() failed."; 
+			$write = $error . "\n"; autolog($write, $target); 
       $report = array("type" => "addA", "error" => $error); 
       echo json_encode ($report); //terminate 
   } else {
+        $write = "executing submitExam().. \n"; autolog($write, $target); 
+				$write = print_r($feedback, $target) . "\n"; autolog($write, $target); 
         echo $feedback; 
   }
       
  function submitExam($conn, $decoder) {
+    
+      $target = '/afs/cad/u/w/b/wbv4/public_html/Middle/tracklogs/addA.txt'; 
       $testId = $decoder['id'];
       $answers = $decoder['answers'];
       $comment = $decoder['comment'];
+      
       $comment = addslashes($comment);
   
     foreach($answers as $x) {
-        $QId = $x['id'];
+        $qId = $x['id'];
         $text = $x['text'];
         $text = addslashes($text);
+
+	$write = "updating database with answer " . $text . "for question " . $qId . "\n"; 
+	autolog($write, $target); 
        // echo "<br<br>text = " . $text . "<br><br>";
-        $sql1 = "UPDATE rd248.QuestionStudentRelation SET userAnswer = '$text' WHERE QuestionStudentRelation.questionId = '$QId' AND 
-        QuestionStudentRelation.testId = '$testId' ";
+        $sql1 = "UPDATE rd248.QuestionStudentRelation SET userAnswer = '$text',	comment = '$comment' WHERE QuestionStudentRelation.questionId = '$qId' AND QuestionStudentRelation.testId = '$testId' ";
         if ( ! $result1 = $conn->query($sql1)) { 
              $sqlerror1 = $conn->error; 
              $error .= "sql1: " . $sqlerror1 . " "; 
+             $write = $error . "\n"; autolog($write);
         } else { 
              /* succesfully updated table */
              /* update sub to 1 */
@@ -50,12 +74,9 @@ if (! $feedback = submitExam($conn, $decoder)) { //calls the function getQUEST()
                     $error .= "sql2: " . $sqlerror2 . " "; 
              } else {  
                   /* succesfully updated table */
-                  
              }//if sql2 
         }//if sql1
-        
     }//foreach answers as x
-    
     if ($error === null) {
             $error = 0; 
     }
@@ -63,6 +84,5 @@ if (! $feedback = submitExam($conn, $decoder)) { //calls the function getQUEST()
     $package = array('type' => 'addA', 'error' => $error, 'id' => $testId, 'sub' => '1', 'comment' => $comment, 'answers' => $answers); 
     return json_encode ($package);
     //return 1; 
-     
- }//submitExam() 
+ }//submitExam()
 ?>
