12007ebad312eb6c546c2c760add7994c5c010ae
warrenjayv <wbv4@njit.edu>
2019-04-07 21:08:05 -0400

    Sun Apr  7 21:08:08 EDT 2019

--- back/targets.php
+++ back/targets.php
@@ -1,25 +1,33 @@
 <?php 
 function targetIs($str) {
 	if ($str == 'addA') {
-		return '/afs/cad/u/w/b/wbv4/public_html/Middle/tracklogs/addA.txt';
+          return 'afs/cad/u/r/d/rd248/public_html/download/beta/log.txt';
 	}
-	if ($str == 'getA') {
-		return '/afs/cad/u/w/b/wbv4/public_html/Middle/tracklogs/getAt.txt';
+	if ($str == 'getA') { 
+          return 'afs/cad/u/r/d/rd248/public_html/download/beta/log.txt';
 	}
+
 	if ($str == 'addT') {
-		return '/afs/cad/u/w/b/wbv4/public_html/Middle/tracklogs/addT.txt';
+          return 'afs/cad/u/r/d/rd248/public_html/download/beta/log.txt';
 	}
 	if ($str == 'getT') {	
-		return '/afs/cad/u/w/b/wbv4/public_html/Middle/tracklogs/getT.txt';
+          return 'afs/cad/u/r/d/rd248/public_html/download/beta/log.txt';
 	}
 	if ($str == 'testB') {
-		return '/afs/cad/u/w/b/wbv4/public_html/Middle/tracklogs/testB.txt';
+          return 'afs/cad/u/r/d/rd248/public_html/download/beta/log.txt';
 	}
 	if ($str == 'quesB') {
-		return '/afs/cad/u/w/b/wbv4/public_html/Middle/tracklogs/quesB.txt';
+          return 'afs/cad/u/r/d/rd248/public_html/download/beta/log.txt';
+	}
+	if ($str == 'quesB') {
+          return 'afs/cad/u/r/d/rd248/public_html/download/beta/log.txt';
 	}
 	if ($str == 'addQ') {
-		return '/afs/cad/u/w/b/wbv4/public_html/Middle/tracklogs/addQ.txt';
+          return 'afs/cad/u/r/d/rd248/public_html/download/beta/log.txt';
 	}
 }
+
+/*
+return 'afs/cad/u/r/d/rd248/public_html/download/beta
+*/
 ?>
