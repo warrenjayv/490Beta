a670db0f49a4779d9287f9888c477a478f7f04ce
raghav dave <rd248@afsconnect1.njit.edu>
2019-04-12 02:22:23 -0400

    Fri Apr 12 02:22:26 EDT 2019

--- back/targets.php
+++ back/targets.php
@@ -1,39 +1,39 @@
 <?php 
 function targetIs($str) {
 	if ($str == 'addA') {
-          return 'afs/cad/u/r/d/rd248/public_html/download/beta/log.txt';
+          return '/afs/cad/u/r/d/rd248/public_html/download/beta/back/log.txt';
 	}
 	if ($str == 'getA') { 
-          return 'afs/cad/u/r/d/rd248/public_html/download/beta/log.txt';
+          return '/afs/cad/u/r/d/rd248/public_html/download/beta/back/log.txt';
 	}
 
 	if ($str == 'addT') {
-          return 'afs/cad/u/r/d/rd248/public_html/download/beta/log.txt';
+          return '/afs/cad/u/r/d/rd248/public_html/download/beta/back/log.txt';
 	}
 	if ($str == 'getT') {	
-          return 'afs/cad/u/r/d/rd248/public_html/download/beta/log.txt';
+          return '/afs/cad/u/r/d/rd248/public_html/download/beta/back/log.txt';
 	}
 	if ($str == 'testB') {
-          return 'afs/cad/u/r/d/rd248/public_html/download/beta/log.txt';
+          return '/afs/cad/u/r/d/rd248/public_html/download/beta/back/log.txt';
 	}
 	if ($str == 'quesB') {
-          return 'afs/cad/u/r/d/rd248/public_html/download/beta/log.txt';
+           return '/afs/cad/u/r/d/rd248/public_html/download/beta/back/log.txt';
 	}
 	if ($str == 'addQ') {
-          return 'afs/cad/u/r/d/rd248/public_html/download/beta/log.txt';
+          return '/afs/cad/u/r/d/rd248/public_html/download/beta/back/log.txt';
 	}
         if ($str == 'auto') {
-       	   return 'afs/cad/u/r/d/rd248/public_html/download/beta/log.txt'; 
+       	   return '/afs/cad/u/r/d/rd248/public_html/download/beta/back/log.txt'; 
         }
 	if ($str == 'getAns') {
-       	   return 'afs/cad/u/r/d/rd248/public_html/download/beta/log.txt'; 
+       	   return '/afs/cad/u/r/d/rd248/public_html/download/beta/back/log.txt'; 
         }
         if ($str == 'getQ') {
-       	   return 'afs/cad/u/r/d/rd248/public_html/download/beta/log.txt'; 
+       	   return '/afs/cad/u/r/d/rd248/public_html/download/beta/back/log.txt'; 
         } 
 }
 
 /*
-return 'afs/cad/u/r/d/rd248/public_html/download/beta/log.txt'; 
-*/
-?>
+return afs/cad/u/r/d/rd248/public_html/download/beta/back/bac; 
+*
+
